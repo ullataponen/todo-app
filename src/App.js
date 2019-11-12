f9ff7ea0494a22dbbdbf391f11c53d4674fcf89d
ullataponen <ulla.taponen@gmail.com>
2019-11-12 13:18:36 +0100

    Add date field, create separate component for Todolist

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
+import Todolist from "./Todolist";
 
 function App() {
-	const [desc, setDesc] = useState("");
-	const [todos, setTodos] = useState([]);
-
-	const inputChanged = event => {
-		setDesc(event.target.value);
-	};
-
-	const addTodo = event => {
-		event.preventDefault();
-		setTodos([...todos, desc]);
-	};
-
 	return (
 		<div className="App">
 			<h1 className="App-header">To Do List</h1>
-			<input type="text" onChange={inputChanged} value={desc} />
-			<button onClick={addTodo}>Add</button>
-			<table>
-				<tbody>
-					{todos.map((todo, index) => (
-						<tr key={index}>
-							<td>{todo}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			<Todolist />
 		</div>
 	);
 }
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,3 +1,51 @@
 import React, { useState } from "react";
 
-return <div></div>;
+const Todolist = () => {
+	const [todo, setTodo] = useState({ desc: "", date: "" });
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = event => {
+		event.preventDefault();
+		setTodos([...todos, todo]);
+	};
+
+	const inputChanged = event => {
+		setTodo({ ...todo, [event.target.name]: event.target.value });
+	};
+
+	return (
+		<div>
+			<input
+				type="date"
+				name="date"
+				onChange={inputChanged}
+				value={todo.date}
+			/>
+			<input
+				type="text"
+				name="desc"
+				onChange={inputChanged}
+				value={todo.desc}
+			/>
+			<button onClick={addTodo}>Add</button>
+			<table>
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Description</th>
+					</tr>
+				</thead>
+				<tbody>
+					{todos.map((todo, index) => (
+						<tr key={index}>
+							<td>{todo.date}</td>
+							<td>{todo.desc}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Todolist;
